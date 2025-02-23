963870cabc6d3e2e1d88cc312ef7f788c55042ab
Kourosh Creat1ve <zahdnia.korosh@gmail.com>
2025-02-23 21:08:30 +0330

    Done Logic

--- Application/Services/CopyUser/UserAccessService.cs
+++ Application/Services/CopyUser/UserAccessService.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Domain.Entities;
 using Application.DataBase;
 using Application.Services.CopyUser;
+using Application.DTO;
 
 public class UserAccessService : IUserAccessService
 {
@@ -14,12 +15,11 @@ public class UserAccessService : IUserAccessService
         _context = context;
     }
 
-    public bool CopyUserAccess(int sourceUserId, int targetUserId)
+    public ResultDto CopyUserAccess(int sourceUserId, int targetUserId , bool copyOnlyView = false)
     {
         // بررسی وجود کاربران
         var sourceUser = _context.ComproUsers.Find(sourceUserId);
         var targetUser = _context.ComproUsers.Find(targetUserId);
-
         if (sourceUser == null || targetUser == null)
         {
             throw new ArgumentException("یکی از کاربران یافت نشد.");
@@ -38,18 +38,18 @@ public class UserAccessService : IUserAccessService
         // حذف دسترسی‌های قبلی کاربر مقصد
         var targetAccessLevels = _context.AccessLevels.Where(a => a.User_Id == targetUserId);
         _context.AccessLevels.RemoveRange(targetAccessLevels);
-
+       
         // کپی کردن دسترسی‌ها
         var newAccessLevels = sourceAccessLevels.Select(a => new AccessLevel
         {
             User_Id = targetUserId,
             AccessLevel_Id= a.AccessLevel_Id,
             AccessView = a.AccessView,
-            AccessNew = a.AccessView,
-            AccessEdit = a.AccessEdit,
-            AccessDelete = a.AccessDelete,
-            AccessFirstConfirm = a.AccessFirstConfirm,
-            AccessSecondConfirm = a.AccessSecondConfirm,
+            AccessNew = copyOnlyView ? false : a.AccessView,
+            AccessEdit = copyOnlyView ? false : a.AccessEdit,
+            AccessDelete = copyOnlyView ? false : a.AccessDelete,
+            AccessFirstConfirm = copyOnlyView ? false : a.AccessFirstConfirm,
+            AccessSecondConfirm = copyOnlyView ? false : a.AccessSecondConfirm,
             AccessPrint = a.AccessPrint,
 
         }).ToList();
@@ -57,7 +57,11 @@ public class UserAccessService : IUserAccessService
         _context.AccessLevels.AddRange(newAccessLevels);
         _context.SaveChanges();
 
-        return true;
+        return new ResultDto
+        {
+            IsSuccess = true,
+            Message = $"انتقال یافت  {targetUser.UserName}  به   {sourceUser.UserName} دسترسی",
+        };
     }
 
 
